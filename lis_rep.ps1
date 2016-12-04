b98e768359607efd6b516e40ad142fe933342c20
laghbabi <samy.laghbabi@hism.fr>
2016-12-04 22:03:59 +0100

    modification du commentaire du script principal lis_rep.ps1

--- lis_rep.ps1
+++ lis_rep.ps1
@@ -1,9 +1,15 @@
 #################################
 ## lister le contenu du repertoire d:\tst_pws
+## export du resultat dans deux fichier csv dans d:\tst_pws\results
+## fichier aa.csv et BB.csv
+## chacun des csv est constitué de deux colonnes
+## une colone nom pour le nom du fichier
+## une colonne chemin pour le chemin du fichier
 #################################
 
 
 $lst = get-childitem d:\tst_pws
-foreach ($id in $lst)  {if ($id.name.substring(0,2) -eq "AA"){write-host "fichier AA" + $id}
+foreach ($id in $lst)  {if ($id.name.substring(0,2) -eq "AA"){write-host "fichier AA" + $id
+                                                              export-csv }
                         if ($id.name.substring(0,2) -eq "BB"){write-host "fichier BB" + $id}
                         }
