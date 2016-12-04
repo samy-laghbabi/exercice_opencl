d02605b6cbde2023c44f40193ecbd54b33abdb53
laghbabi <samy.laghbabi@hism.fr>
2016-12-04 21:59:01 +0100

    creation de la command initial qui permet de lister le repertoire et lister les fichiers AA et BB

--- lis_rep.ps1
+++ lis_rep.ps1
@@ -3,4 +3,7 @@
 #################################
 
 
-get-content d:\tst_pws
+$lst = get-childitem d:\tst_pws
+foreach ($id in $lst)  {if ($id.name.substring(0,2) -eq "AA"){write-host "fichier AA" + $id}
+                        if ($id.name.substring(0,2) -eq "BB"){write-host "fichier BB" + $id}
+                        }
